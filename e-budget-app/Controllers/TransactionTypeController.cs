afb6145d40a492512b67e701f27f46ed45e672d2
Uzair Ahmed <uzairahmed4705@gmail.com>
2024-06-28 18:50:27 +0500

    initial commit for transaction type controller

---
+++ e-budget-app/Controllers/TransactionTypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace e_budget_app.Controllers
+{
+    public class TransactionTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
