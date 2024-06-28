098192d28e969b909c8ba8c1f467ead541b3ce34
Uzair Ahmed <uzairahmed4705@gmail.com>
2024-06-28 17:04:18 +0500

    added budget-controller

---
+++ e-budget-app/Controllers/BudgetController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace e_budget_app.Controllers
+{
+    [ApiController]
+    [Route("api/budget")]
+    public class BudgetController : ControllerBase
+    {
+    }
+}
