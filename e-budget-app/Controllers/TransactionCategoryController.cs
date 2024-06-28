bde06944728aafd73fcae566417a7610672dc23d
Uzair Ahmed <uzairahmed4705@gmail.com>
2024-06-28 17:39:30 +0500

    added transaction categories

---
+++ e-budget-app/Controllers/TransactionCategoryController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using e_budget_app.Entities;
+
+namespace e_budget_app.Controllers
+{
+    [ApiController]
+    [Route("api/transactioncategory")]
+    public class TransactionCategoryController : ControllerBase
+    {
+        private readonly string? _connectionString;
+        public TransactionCategoryController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        [HttpGet]
+        public IActionResult GetAllTransactionCategories()
+        {
+            List<TransactionCategory> transactionCategories = new List<TransactionCategory>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    try
+                    {
+                        using SqlCommand cmd = new SqlCommand("Select * From TransactionCategory", connection);
+                        connection.Open();
+                        using (SqlDataReader sqlDataReader = cmd.ExecuteReader())
+                        {
+                            while (sqlDataReader.Read())
+                            {
+                                TransactionCategory transactionCategory = new TransactionCategory
+                                {
+                                    Id = (int)sqlDataReader["Id"],
+                                    Name = sqlDataReader["Name"].ToString()
+                                };
+                                transactionCategories.Add(transactionCategory);
+                            }
+                        }
+                        return Ok(transactionCategories);
+                    }
+                    catch (Exception ex)
+                    {
+                        return BadRequest(ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateTransactionCategory()
+        {
+
+        }
+    }
+}
---
+++ e-budget-app/Entities/TransactionCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace e_budget_app.Entities
+{
+    public class TransactionCategory
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+    }
+}
