f6a15dfa5575d095d4c4217476193f280e652db0
Uzair Ahmed <uzairahmed4705@gmail.com>
2024-06-28 16:20:04 +0500

    added user entity

---
+++ e-budget-app/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace e_budget_app.Entities
+{
+    public class User
+    {   
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Gmail { get; set; }
+    }
+}
---
+++ e-budget-app/Utilities/Dtos/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace e_budget_app.Utilities.Dtos
+{
+    public class UserDto
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Gmail { get; set; }
+    }
+}
